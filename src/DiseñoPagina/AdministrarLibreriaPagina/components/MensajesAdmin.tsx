c8b64ba19f0927e9b09a69811db0b96386a6ef8d
Electro-kerubin <rbcirano.90@gmail.com>
2023-05-13 17:44:02 -0400

    respuesta de mensajes en admin funcionalidad lista

--- src/DiseñoPagina/AdministrarLibreriaPagina/components/MensajesAdmin.tsx
+++ src/DiseñoPagina/AdministrarLibreriaPagina/components/MensajesAdmin.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import MensajesModelo from "../../../Modelos/MensajesModelo";
 import { SpinnerLoading } from "../../Utilidad/SpinnerLoading";
 import MensajesAdminRequestModelo from "../../../Modelos/MensajesAdminRequestModelo";
+import { ResponderMensajesForm } from "./ResponderMensajesForm";
+import { Paginador } from "../../Utilidad/Paginador";
 
 export const MensajesAdmin = () => {
 
@@ -15,7 +17,7 @@ export const MensajesAdmin = () => {
     const [httpError, setHttpError] = useState(null);
 
     const [paginaActual, setPaginaActual] = useState(1);
-    const [paginasTotal, setPaginasTotal] = useState(0);
+    const [totalPaginas, setTotalPaginas] = useState(0);
 
     const [formWarning, setFormWarning] = useState(false);
     const [formResponse, setFormResponse] = useState('');
@@ -26,7 +28,6 @@ export const MensajesAdmin = () => {
 
     //
     useEffect(() => {
-
         const fetchRetornaMensajes = async () => {
             if (authState && authState.isAuthenticated) {
                 const apiUrl = `http://localhost:8080/api/mensajeses/search/findByCerrado/?cerrado=false&page=${paginaActual - 1}$size=${mensajesPorPagina}`;
@@ -46,7 +47,7 @@ export const MensajesAdmin = () => {
                 const fetchRetornaMensajesResponseJson = await fetchRetornaMensajesResponse.json();
 
                 setMensajes(fetchRetornaMensajesResponseJson._embedded.mensajeses);
-                setPaginasTotal(fetchRetornaMensajesResponseJson.page.totalPages);
+                setTotalPaginas(fetchRetornaMensajesResponseJson.page.totalPages);
             }
 
             setMensajesCargados(false);
@@ -74,8 +75,8 @@ export const MensajesAdmin = () => {
         );
     }
 
-    async function responderPregunta(id: number, respuesta: string) {
-        const apiUrl = `http://localhost:3000/api/mensajeses/confidencial/admin/mensaje`;
+    async function enviarRespuesta(id: number, respuesta: string) {
+        const apiUrl = `http://localhost:8080/api/mensajeses/confidencial/admin/mensaje`;
         if (authState && authState?.isAuthenticated && id !== null && respuesta !== '') {
 
             const mensajesAdminRequestModel: MensajesAdminRequestModelo = new MensajesAdminRequestModelo(id, respuesta);
@@ -96,15 +97,6 @@ export const MensajesAdmin = () => {
         }
     }
 
-    function respuestaEnviadaButton(id: any, respuesta: any) {
-        if (id !== null && respuesta !== '') {
-            responderPregunta(id, respuesta);
-            setFormWarning(false);
-        } else {
-            setFormWarning(true);
-        }
-    }
-
     const paginador = (numPag: number) => setPaginaActual(numPag);
 
     return (
@@ -113,38 +105,13 @@ export const MensajesAdmin = () => {
                 <>
                     <h5>Preguntas:</h5>
                     {mensajes.map(mensaje => (
-                        <div className="card mt-2 shadow p-3 bg-body rounded">
-                            <h5>Preguta #{mensaje.id}</h5>
-                            <h6>{mensaje.usuarioEmail}</h6>
-                            <p>{mensaje.pregunta}</p>
-                            <hr />
-                            <div>
-                                <h5>Respuesta:</h5>
-                                <form action="PUT">
-                                    {formWarning &&
-                                        <div className="alert alert-danger" role="alert">
-                                            Todos los campos deben estar completos.
-                                        </div>
-                                    }
-                                    <div className="col-md-12 mb-3">
-                                        <label className="form-label">Descripcion</label>
-                                        <textarea className="form-control" id="responderMensajesFormId" rows={3}
-                                            onChange={e => setFormResponse(e.target.value)} value={formResponse}></textarea>
-                                    </div>
-                                    <div>
-                                        <button type="button" className="btn btn-primary mt-3" onClick={() => respuestaEnviadaButton(mensaje.id, mensaje.respuesta)}> Enviar</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-
+                        <ResponderMensajesForm mensaje={mensaje} key={mensaje.id} enviarRespuesta={enviarRespuesta} />
                     ))}
-
-
                 </>
                 :
                 <h5>No hay preguntas pendientes.</h5>
             }
+            {totalPaginas > 1 && <Paginador paginaActual={paginaActual} totalPaginas={totalPaginas} paginador={paginador} />}
         </div>
     );
 }
